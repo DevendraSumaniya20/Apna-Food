4c7700ee8cae0238915e0175e52ba885aa15dbd3
Sumaniya Devendra <dsumniya@gmail.com>
2023-04-21 13:57:32 +0530

    update the changes

--- source/screens/MapScreen/MapScreen.js
+++ source/screens/MapScreen/MapScreen.js
@@ -98,69 +98,71 @@ const MapScreen = ({route, navigation}) => {
       />
 
       <View style={styles.main}>
-        <MapView
-          style={styles.map}
-          initialRegion={region}
-          showsUserLocation={true}>
-          <Circle
-            center={{latitude, longitude}}
-            radius={moderateScale(10)}
-            fillColor={colors.mainThemesColorOp}
-            strokeWidth={moderateScale(0.01)}
-            zIndex={moderateScale(0)}
-          />
-          <Circle
-            center={{latitude, longitude}}
-            radius={moderateScale(4)}
-            fillColor={colors.white}
-            strokeColor={colors.mainThemesColor}
-            strokeWidth={moderateScale(30)}
-            zIndex={moderateScale(2)}
-          />
-          <Polyline
-            coordinates={coordinate}
-            strokeColor={colors.mainThemesColor}
-            strokeWidth={moderateScale(5)}
-          />
+        {latitude && longitude && (
+          <MapView
+            style={styles.map}
+            initialRegion={region}
+            showsUserLocation={true}>
+            <Circle
+              center={{latitude, longitude}}
+              radius={moderateScale(10)}
+              fillColor={colors.mainThemesColorOp}
+              strokeWidth={moderateScale(0.01)}
+              zIndex={moderateScale(0)}
+            />
+            <Circle
+              center={{latitude, longitude}}
+              radius={moderateScale(4)}
+              fillColor={colors.white}
+              strokeColor={colors.mainThemesColor}
+              strokeWidth={moderateScale(30)}
+              zIndex={moderateScale(2)}
+            />
+            <Polyline
+              coordinates={coordinate}
+              strokeColor={colors.mainThemesColor}
+              strokeWidth={moderateScale(5)}
+            />
 
-          <Marker
-            draggable={true}
-            coordinate={{latitude, longitude, title, rating, image}}
-            image={ImagePath.ShopIcon}
-            title={title}
-            rating={rating}
-            tappable={true}>
-            <Callout tooltip>
-              <View style={styles.MapTopscreen}>
-                <View>
-                  <Image
-                    resizeMode="contain"
-                    style={styles.MapShopImg}
-                    source={ImagePath.mapShopTitleImg}
-                  />
-                </View>
-                <View style={styles.MapTopscreenSecondView}>
-                  <Text style={styles.TextTopTitle}>{title}</Text>
-                  <View style={styles.ratingStyle}>
-                    {generateRatingStars({rating})}
+            <Marker
+              draggable={true}
+              coordinate={{latitude, longitude, title, rating, image}}
+              image={ImagePath.ShopIcon}
+              title={title}
+              rating={rating}
+              tappable={true}>
+              <Callout tooltip>
+                <View style={styles.MapTopscreen}>
+                  <View>
+                    <Image
+                      resizeMode="contain"
+                      style={styles.MapShopImg}
+                      source={ImagePath.mapShopTitleImg}
+                    />
+                  </View>
+                  <View style={styles.MapTopscreenSecondView}>
+                    <Text style={styles.TextTopTitle}>{title}</Text>
+                    <View style={styles.ratingStyle}>
+                      {generateRatingStars({rating})}
+                    </View>
                   </View>
                 </View>
-              </View>
 
-              <View style={styles.CalloutArrow} />
-            </Callout>
-          </Marker>
+                <View style={styles.CalloutArrow} />
+              </Callout>
+            </Marker>
 
-          <Circle
-            center={{latitude, longitude}}
-            radius={moderateScale(10)}
-            strokeColor={colors.mainThemesColor}
-            fillColor={colors.color1Home}
-            strokeWidth={moderateScale(1)}
-            lineCap={'square'}
-            lineJoin={'bevel'}
-          />
-        </MapView>
+            <Circle
+              center={{latitude, longitude}}
+              radius={moderateScale(10)}
+              strokeColor={colors.mainThemesColor}
+              fillColor={colors.color1Home}
+              strokeWidth={moderateScale(1)}
+              lineCap={'square'}
+              lineJoin={'bevel'}
+            />
+          </MapView>
+        )}
       </View>
     </View>
   );
