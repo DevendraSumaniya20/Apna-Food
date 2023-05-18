795cefab37c76523c3b11a73243da6697aafdeb3
Sumaniya Devendra <dsumniya@gmail.com>
2023-05-18 18:11:35 +0530

    add new changes

--- source/constant/ImagePath.js
+++ source/constant/ImagePath.js
@@ -2,7 +2,6 @@ export default {
   FoodApp: require('../assets/images/FoodIcon.jpg'),
   showEye: require('../assets/images/showEye.png'),
   hideEye: require('../assets/images/hideEye.jpeg'),
-  FoodIcon: require('../assets/images/FoodIcon.png'),
   locationIcon: require('../assets/images/map.png'),
   startFillIcon: require('../assets/images/Star-fill.png'),
   startEmptyIcon: require('../assets/images/Star-empty.png'),
